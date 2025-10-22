5a1d4bef892a01e19ae47df4908d3ebe371f29bb
pamelladsco <pamellacarraro@hotmail.com>
2025-10-22 20:38:01 -0300

    Desafios Trabalho Avaliativo

--- script.js
+++ script.js
@@ -26,11 +26,15 @@ const forcaEncatamento = 1.5 //(Multiplicador)
 const armaduraInimiga = 2 //(Redução de Dano)
 let vidaInimiga = 20 //(Vida do Creeper)
 
-let danoBruto = danoEspada * forcaEncatamento
+//Utilizados operadores aritméticos para calcular o dano
+let danoBruto = danoEspada * forcaEncatamento 
 let danoFinal = danoBruto - armaduraInimiga
 
+//Operador aritmético simplificado, calculando a vida após dano
 vidaInimiga -= danoFinal
 
+//Se o valor da variável vidaInimiga for menor ou igual a zero, será executado a mensagem de if.
+//Caso não, será impresso a mensagem de else. 
 if(vidaInimiga <= 0){
     console.log('O Creeper foi derrotado! Você sobreviveu à explosão!')
 } else {
@@ -38,14 +42,18 @@ if(vidaInimiga <= 0){
 }
 
 //Desafio 3
+//Criadas variáveis com tipos booleanos e number.
 
 const temDiamantes = false
 let nivelMesaCrafting = 2
 const temGravetos = true
 let temFerro = 8
 
+//Condições necessárias para que o Crafting seja permitido, será possível caso não tenha gravetos ou o nível da mesa de crafting seja menor que 3, além de ter diamantes ou ter 8 ou mais ferros.
+
 const condicional = ((temGravetos === false) || (nivelMesaCrafting < 3)) && ((temDiamantes === true) || (temDiamantes >= 8)) 
 
+// Utilizada a estrutura condicional if e else, sendo que se todas as variáveis forem true, será impresso no console a mensagem de if, e caso contrário a mensagem de else. 
 if(condicional){
     console.log('Picareta de Diamante criada! Hora de buscar Obsidiana!')
 } else{
